5597b69aaa75d4dd173bcb2170a9722c9d6cafe9
Ashish Uttam <auttam@outlook.com>
2019-09-20 10:22:13 -0500

    Add required decorator & export all decorators

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
+// Base Program Class
 export { Program } from './lib/program'
+
+// Cli and Command Decorators
+export { Cli, Command, required } from './lib/decorators'
--- src/lib/decorators/index.ts
+++ src/lib/decorators/index.ts
@@ -1,2 +1,3 @@
 export { programDecoratorFactory as Cli } from './program-decorator'
 export { commandDecoratorFactory as Command } from './command-decorator'
+export { required } from './required-param-decorator'
